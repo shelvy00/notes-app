36660c1548acb3696a705a363dd0fe9dea2f033a
Shelvy <shelvy511@gmail.com>
2025-04-12 13:21:23 -0500

    updated HomeScreen

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,7 +1,24 @@
 import { Stack } from "expo-router";
 
 const RootLayout = () => {
-  return <Stack />;
+  return <Stack 
+  screenOptions={{
+    headerStyle:{
+      backgroundColor: '#ff8c00',
+    },
+    headerTintColor: '#fff',
+    headerTitleStyle: {
+      fontSize: 20,
+      fontWeight: "bold",
+    },
+    contentStyle: {
+      paddingHorizontal: 10,
+      paddingTop: 10,
+      backgroundColor: '#fff',
+    },
+  }}>
+    <Stack.Screen name="index" options={{title: "Home"}} />
+  </Stack>  
 }
 
 export default RootLayout;
