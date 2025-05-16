9c7ddc33a542e3e01b643c1bf06df0d408f86f2c
Shelvy <shelvy511@gmail.com>
2025-05-15 22:25:30 -0500

    authService

--- services/appwrite.js
+++ services/appwrite.js
@@ -1,4 +1,4 @@
-import { Client, Databases } from "react-native-appwrite";
+import { Client, Databases, Account } from "react-native-appwrite";
 import { Platform } from "react-native";
 
 const config = {
@@ -25,4 +25,6 @@ switch (Platform.OS) {
 
 const database = new Databases(client);
 
-export { database, config, client };
+const account = new Account(client);
+
+export { database, config, client, account };
