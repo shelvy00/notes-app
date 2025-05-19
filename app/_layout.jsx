a895477bca6a873c8e21c44f1e193d06181d6075
Shelvy <shelvy511@gmail.com>
2025-05-18 22:38:00 -0500

    Auth Screen

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,4 +1,5 @@
 import { Stack } from "expo-router";
+import { AuthProvider } from '@/contexts/AuthContext';
 
 const RootLayout = () => {
   return <Stack 
--- app/notes/index.jsx
+++ app/notes/index.jsx
@@ -1,10 +1,15 @@
 import { useState, useEffect } from "react";
 import { View, Text, StyleSheet,TouchableOpacity, Alert, ActivityIndicator} from "react-native";
+import { useRouter } from "expo-router";
+import { useAuth } from "@/contexts/AuthContext";
 import NoteList from "@/components/NoteList";
 import AddNoteModal from "@/components/AddNoteModal";
 import noteService from "@/services/noteService";
 
 const NoteScreen = () => {
+  const router = useRouter();
+  const { user, loading:authLoading } = useAuth;
+
    const [notes, setNotes] = useState([]);
    const [modalVisible, setModalVisible] = useState(false);
    const [newNote, setNewNote] = useState("");
@@ -12,8 +17,16 @@ const NoteScreen = () => {
    const [error, setError] = useState(null);
 
    useEffect(() => {
-    fetchNotes();
-   },[]);
+    if (!authLoading && !user) {
+      router.replace('/auth');
+    }
+  }, [user, authLoading]);
+
+   useEffect(() => {
+    if (user) {
+      fetchNotes();
+    }
+   },[user]);
 
    const fetchNotes = async () => {
       setLoading(true);
